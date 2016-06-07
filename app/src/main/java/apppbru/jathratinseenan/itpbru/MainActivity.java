1ee056106f0ae28669a7881a596dbc3e0c45ce57
Shopping <shop121236@gmail.com>
2016-06-07 11:05:42 +0700

    Design Space Finish

--- app/src/main/java/apppbru/jathratinseenan/itpbru/MainActivity.java
+++ app/src/main/java/apppbru/jathratinseenan/itpbru/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     }   // Main Method
 
     public void clickSignUpMain(View view) {
-        startActivity(new Intent(MainActivity.this, SignUpActivity.class););
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
     }
 
 }   // Main class นี่คือ คลาสหลัก
