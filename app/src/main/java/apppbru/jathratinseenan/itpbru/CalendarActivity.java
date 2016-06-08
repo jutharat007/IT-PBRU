18ad5d2651d8fbff48a091c9a1199845a9090e14
Shopping <shop121236@gmail.com>
2016-06-08 13:59:54 +0700

    Design UI Upload

--- app/src/main/java/apppbru/jathratinseenan/itpbru/CalendarActivity.java
+++ app/src/main/java/apppbru/jathratinseenan/itpbru/CalendarActivity.java
@@ -2,6 +2,7 @@ package apppbru.jathratinseenan.itpbru;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CalendarView;
@@ -51,11 +52,20 @@ public class CalendarActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
 
+                Intent intent = new Intent(CalendarActivity.this, UploadAccount.class);
+                intent.putExtra("Login", getIntent().getStringArrayExtra("Login"));
+                intent.putExtra("InOut", 0);
+                startActivity(intent);
+
             }
         });
         builder.setNeutralButton("รายจ่าย", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+
+                Intent intent = new Intent(CalendarActivity.this, UploadAccount.class);
+                intent.putExtra("Login", getIntent().getStringArrayExtra("Login"));
+                startActivity(intent);
 
             }
         });
