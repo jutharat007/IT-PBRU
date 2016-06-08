0cfc4c555c6bb74adf6deeee9a60aec961b6a696
Shopping <shop121236@gmail.com>
2016-06-08 12:08:15 +0700

    Before Luech

---
+++ app/src/main/java/apppbru/jathratinseenan/itpbru/CalendarActivity.java
@@ -0,0 +1,66 @@
+package apppbru.jathratinseenan.itpbru;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    // Explicit
+    private CalendarView calendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+        // Bind Widget
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+
+        // Active When Click Calender
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
+                confirmDialog(day, month, year);
+
+            }
+        });
+
+    }  // Main Method
+
+    private void confirmDialog(int day, int month, int year) {
+
+        String strDate = Integer.toString(day) +
+                "/" + Integer.toString(month + 1) +
+                "/" + Integer.toString(year);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.nobita48);
+        builder.setTitle(strDate);
+        builder.setMessage("คุณต้องการ บันทึก รายรับ หรือ รายจ่าย ?");
+        builder.setNegativeButton("Cancle", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setPositiveButton("รายรับ", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setNeutralButton("รายจ่าย", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.show();
+
+    } // confirmDialog
+
+}  // Main Class
